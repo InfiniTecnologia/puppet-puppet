bdfcdfaf7bccd9c803430e7484c8279e90950d1f
Raphael Neumann <raphael@infinitecnologia.com.br>
2018-04-02 00:31:25 -0400

    Create defalt params and class to install puppetser

---
+++ manifests/server.pp
@@ -0,0 +1,33 @@
+# == Class: puppet:server
+#
+# This is will install and configure puppet-server daemon
+# === Requirements
+#
+#
+# === Parameters
+#
+#
+# === Example
+#
+#
+# === Authors
+#
+# Author Name: Raphael Neumann <raphael@infinitecnologia.com.br>
+#
+# === Copyright
+#
+# Copyright 2018 Infini Tecnologia
+#
+class puppet::server(
+	$version,
+	$enable,
+	$package,
+	$service,
+	$manage_service,
+	) {
+	
+	include puppet::server::install
+
+	#Class['puppet::server::install']
+
+}
