ea30a23ed689c044ea81e6e91d357fe08655ab63
Rafael Sales <rafael@infinitecnologia.com.br>
2018-04-08 02:25:45 -0300

    fix class puppet::agent::configure

--- manifests/agent/configure.pp
+++ manifests/agent/configure.pp
@@ -21,7 +21,7 @@
 class puppet::agent::configure{
 
   $puppet_config_defaults = lookup('puppet::agent::puppet_config');
-  $puppet_config = deep_merge($puppet_config_defaults, $conf_autosign, $puppet::agent::puppet_config_override_defaults)
+  $puppet_config = deep_merge($puppet_config_defaults, $puppet::agent::puppet_config_override_defaults)
 
   file {'puppet/puppet.conf':
     path    => '/etc/puppetlabs/puppet/puppet.conf',
@@ -29,9 +29,4 @@ class puppet::agent::configure{
     mode    => '0644',
   }
 
-  #augeas {'java_args':
-  #  context => "${puppet::agent::path_system_config}/puppetagent",
-  #  changes => [ "set JAVA_ARGS '\"${puppetagent::java_args}\"'", ],
-  #  notify  => Service['puppetagent']
-  #}
 }
