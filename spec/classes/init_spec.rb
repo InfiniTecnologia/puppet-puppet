487b8a1baccd925131c101c3e753786aa1c0bd0e
Rafael Sales <rafael@infinitecnologia.com.br>
2018-04-20 05:32:28 +0000

    Write more tests for class puppet init

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -10,4 +10,34 @@ describe 'puppet', :type => :class do
   it { is_expected.to contain_class('puppet::agent::service') }
 
 
+  context 'when not managing the service' do
+    let(:params) { { manage_service: false } }
+      it { is_expected.to compile.with_all_deps }
+      it { is_expected.not_to contain_service('puppet') }
+  end
+
+  context 'when managing the service' do
+    let(:params) { { manage_service: true } }
+      it { is_expected.to compile.with_all_deps }
+      it { is_expected.to contain_service('puppet') }
+  end
+
+
+  context 'override config new values' do
+    let(:params)  { { puppet_config_override_defaults: { 'main' => { 'certname': 'agent.server', 'server': 'server', 'environment': 'dev', 'runinterval' => '1m' } } } }
+      it { is_expected.to contain_file('puppet/puppet.conf').with_content(%r{^certname = agent.server}) }
+      it { is_expected.to contain_file('puppet/puppet.conf').with_content(%r{^server = server}) }
+      it { is_expected.to contain_file('puppet/puppet.conf').with_content(%r{^environment = dev}) }
+      it { is_expected.to contain_file('puppet/puppet.conf').with_content(%r{^runinterval = 1m}) }
+  end
+
+
+  context 'override config false' do
+    let(:facts) { {'fqdn' => 'agent.lab', 'domain' => 'lab'} }
+      it { is_expected.to contain_file('puppet/puppet.conf').with_content(%r{^certname = agent.lab}) }
+      it { is_expected.to contain_file('puppet/puppet.conf').with_content(%r{^server = puppet.lab}) }
+      it { is_expected.to contain_file('puppet/puppet.conf').with_content(%r{^environment = production}) }
+      it { is_expected.to contain_file('puppet/puppet.conf').with_content(%r{^runinterval = 30m}) }
+  end
+
 end
